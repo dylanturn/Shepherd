366f1ad1e528e46fba192ff451854a535d921d35
Dylan Turnbull <dylanturn@gmail.com>
2016-11-13 13:52:25 -0600

    as

--- index.php
+++ index.php
@@ -65,7 +65,7 @@ try{
 ?>
 
 <html>
-<title>Shepherd v1</title>
+<title>Shepherd</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="lib/w3.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
